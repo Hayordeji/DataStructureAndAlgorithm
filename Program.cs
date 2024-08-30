5c7372c56f8c764c74228c23ac4426773c3cc143
Shoga Ayodeji <86511436+Hayordeji@users.noreply.github.com>
2024-08-30 17:00:42 +0100

    Another lesson

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ int[] intArray = new int[8];
 int length = 0;
 
 ///fill the array
-for (int i = 0; i < 6; i++)
+for (int i = 0; i < 8; i++)
 {
     intArray[length] = i + 1;
     length++;
@@ -19,11 +19,26 @@ for (int i = 0; i < 6; i++)
 
 
 //ARRAY INSERTION AT START
+////move each elements in an array down 
+//for (int i = 6; i >= 0; i--)
+//{
+//    intArray[i + 1] = intArray[i];
+//}
+//intArray[0] = 8;
+////dummy code
+//int value = 0;
 
-//move each elements in an array down 
-for (int i = 6; i >= 0; i--)
+//LINEAR SEARCH
+bool LinearSearch(int[] intArray, int key)
 {
-    intArray[i + 1] = intArray[i];
+    for (int i = 0;i < intArray.Length;i++)
+    {
+        if (intArray[i] == key)
+        {
+            return true;
+        }
+    }
+    return false;
 }
-intArray[0] = 8;
-int value = 0;
+
+Console.WriteLine(LinearSearch(intArray, 5));
