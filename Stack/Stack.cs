fd3eb1fccb1e8f3bb3f819f067ab44664ca1e500
Shoga Ayodeji <86511436+Hayordeji@users.noreply.github.com>
2024-09-02 10:04:10 +0100

    Create Stack

---
+++ Stack/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAndAlgorithm.Stack
+{
+    public class Stack
+    {
+        public int MaxSize { get; set; }
+        public string[] StackArray { get; set; }
+        public int Top { get; set; }
+
+        public Stack(int size)
+        {
+            this.MaxSize = size;
+            this.StackArray = new string[size];
+            this.Top = -1;
+        }
+
+        public void Push(string text)
+        {
+            Top++;
+            StackArray[Top] = text;
+        }
+
+        public string Pop() 
+        {
+            int old_top = Top;
+            Top--;
+            return StackArray[old_top];
+        }
+
+        public string Peek()
+        {
+            return StackArray[Top];
+        }
+    }
+}
